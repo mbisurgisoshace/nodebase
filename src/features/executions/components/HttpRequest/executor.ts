90f978b91fabcf94833f04df35264fc08d8c9e81
mbisurgisoshace <maximiliano.bisurgi@gmail.com>
2026-02-27 15:34:13 -0300

    20: note templating

--- src/features/executions/components/HttpRequest/executor.ts
+++ src/features/executions/components/HttpRequest/executor.ts
@@ -1,6 +1,14 @@
+import Handlebars from "handlebars";
 import { NonRetriableError } from "inngest";
 import ky, { type Options as KyOptions } from "ky";
+
 import type { NodeExecutor } from "@/features/executions/types";
+
+Handlebars.registerHelper("json", (context) => {
+  const jsonString = JSON.stringify(context, null, 2);
+  const safeString = new Handlebars.SafeString(jsonString);
+  return safeString;
+});
 
 type HttpRequestData = {
   body?: string;
@@ -20,14 +28,16 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
   }
 
   const result = await step.run("http-request", async () => {
-    const endpoint = data.endpoint!;
+    const endpoint = Handlebars.compile(data.endpoint)(context);
     const method = data.method || "GET";
 
     const options: KyOptions = { method };
 
     if (["POST", "PUT", "PATCH"].includes(method)) {
       if (data.body) {
-        options.body = data.body;
+        const resolved = Handlebars.compile(data.body || "{}")(context);
+        JSON.parse(resolved);
+        options.body = resolved;
         options.headers = {
           "Content-Type": "application/json",
         };
